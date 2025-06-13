fde08124acffd33bad370403c8c74643dbb7a244
신동훈 <easthoon1996@gmail.com>
2025-06-13 15:40:24 +0900

    Initial commit

---
+++ src/main/java/com/test/iam/NewiamApplication.java
@@ -0,0 +1,13 @@
+package com.test.iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewiamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewiamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/iam/NewiamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewiamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
